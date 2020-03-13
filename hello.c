91bd935696f6ac493896dd269c6018fa267270ea
patilana22 <44269037+patilana22@users.noreply.github.com>
2020-03-13 10:49:07 -0400

    Create hello.c
    This is the best version of hello world, thank you.

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+//Anand Patil. All reproduction of code is unauthorized under the Creative Commons License!!1
+
+int main(void)
+{
+    //I print the statement
+    printf("Hello, world!\n");
+}
